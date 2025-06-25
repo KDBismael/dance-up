5e8c317f621a3da40e9307e06b0bafd113ee610a
KDBismael <ismaelkondombo1@gmail.com>
2025-06-25 16:39:48 +0000

    Add guest profile feature screens

--- lib/features/profile/screens/guest_profile.dart
+++ lib/features/profile/screens/guest_profile.dart
@@ -1,9 +1,12 @@
 import 'package:dance_up/core/components/base_widget_with_gradient.dart';
+import 'package:dance_up/core/components/custom_button.dart';
+import 'package:dance_up/core/components/custon_modal_bottom_sheet.dart';
 import 'package:dance_up/core/theme/colors.dart';
 import 'package:dance_up/features/auth/auth_presenter.dart';
 import 'package:dance_up/routes/get_pages.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class GuestProfile extends StatelessWidget {
   GuestProfile({super.key});
@@ -41,7 +44,8 @@ class GuestProfile extends StatelessWidget {
                 () => Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Text("KDB Ismael",
+                    Text(
+                        "KDB Ismael ${authPresenter.user.value?.username ?? ''}",
                         style: Theme.of(context)
                             .textTheme
                             .displayLarge
@@ -113,6 +117,7 @@ class GuestProfile extends StatelessWidget {
                           onPressed: () {},
                           style: OutlinedButton.styleFrom(
                             backgroundColor: AppColors.accent,
+                            // backgroundColor: AppColors.red,
                             side: BorderSide.none,
                             minimumSize: Size.zero,
                             shape: RoundedRectangleBorder(
@@ -123,6 +128,7 @@ class GuestProfile extends StatelessWidget {
                           ),
                           child: Text(
                             "Follow",
+                            // "Unfollow",
                             style: Theme.of(context)
                                 .textTheme
                                 .labelMedium
@@ -132,7 +138,35 @@ class GuestProfile extends StatelessWidget {
                         // const SizedBox(width: 10),
                         const Spacer(),
                         OutlinedButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            showCustomModalBottomSheet(
+                              context: context,
+                              child: SendPartnerRequestModal(
+                                  authPresenter: authPresenter),
+                            );
+                            // showCustomModalBottomSheet(
+                            //   context: context,
+                            //   child: RemoveOrCancelRequestModal(
+                            //     authPresenter: authPresenter,
+                            //     title: 'Cancel partner request to Jaydon',
+                            //     btnText: "wait for response",
+                            //     subtitle:
+                            //         'Are you sure you want to cancel your partner request sended ?',
+                            //     onPressed: () {},
+                            //   ),
+                            // );
+                            // showCustomModalBottomSheet(
+                            //   context: context,
+                            //   child: RemoveOrCancelRequestModal(
+                            //     authPresenter: authPresenter,
+                            //     title: 'Remove partnership with Jaydon',
+                            //     btnText: "Cancel",
+                            //     subtitle:
+                            //         'Are you sure you want to remove your partnership with Jaydon ?',
+                            //     onPressed: () {},
+                            //   ),
+                            // );
+                          },
                           style: OutlinedButton.styleFrom(
                             backgroundColor: AppColors.background,
                             side: BorderSide.none,
@@ -158,6 +192,15 @@ class GuestProfile extends StatelessWidget {
                             Icons.send_outlined,
                             color: AppColors.accent,
                             size: 18,
+                          ),
+                          style: TextButton.styleFrom(
+                            side: BorderSide.none,
+                            minimumSize: Size.zero,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(4),
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 3.5, horizontal: 8.5),
                           ),
                           label: Text(
                             'Share',
@@ -217,6 +260,169 @@ class GuestProfile extends StatelessWidget {
             ),
           ),
         )
+      ],
+    );
+  }
+}
+
+class SendPartnerRequestModal extends StatelessWidget {
+  const SendPartnerRequestModal({
+    super.key,
+    required this.authPresenter,
+  });
+
+  final AuthPresenter authPresenter;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          "Send partner request to KDB Ismael",
+          style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                fontFamily: GoogleFonts.getFont('Roboto Condensed').fontFamily,
+                fontSize: 20,
+                color: AppColors.black,
+              ),
+        ),
+        const SizedBox(height: 4),
+        Text(
+          "Send a partner request to dancers nearby and start planning your next dance meetup!",
+          style: Theme.of(context).textTheme.labelSmall?.copyWith(
+                color: AppColors.blackGray,
+              ),
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 31),
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Custom message (optional)",
+            hintStyle: Theme.of(context)
+                .textTheme
+                .labelSmall
+                ?.copyWith(color: AppColors.blackGray),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: const BorderSide(
+                color: AppColors.gray,
+                width: 0.5,
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: const BorderSide(
+                color: AppColors.black,
+                width: 1,
+              ),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: const BorderSide(
+                color: AppColors.gray,
+                width: 0.5,
+              ),
+            ),
+          ),
+          maxLines: 4,
+        ),
+        const SizedBox(height: 22),
+        Obx(
+          () => CustomButton(
+            text: "Submit",
+            isLoading: authPresenter.isLoading.value,
+            onPressed: () async {
+              // await authPresenter.sendPartnerRequest();
+              // Get.back();
+            },
+          ),
+        ),
+        const SizedBox(height: 16),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Text(
+                "Cancel",
+                style: Theme.of(context)
+                    .textTheme
+                    .labelMedium
+                    ?.copyWith(color: AppColors.blackGray),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class RemoveOrCancelRequestModal extends StatelessWidget {
+  const RemoveOrCancelRequestModal({
+    super.key,
+    required this.authPresenter,
+    required this.title,
+    required this.subtitle,
+    this.onPressed,
+    required this.btnText,
+  });
+
+  final AuthPresenter authPresenter;
+  final String title;
+  final String subtitle;
+  final String btnText;
+  final void Function()? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          title,
+          style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                fontFamily: GoogleFonts.getFont('Roboto Condensed').fontFamily,
+                fontSize: 20,
+                color: AppColors.black,
+              ),
+        ),
+        const SizedBox(height: 4),
+        Text(
+          subtitle,
+          style: Theme.of(context).textTheme.labelSmall?.copyWith(
+                color: AppColors.blackGray,
+              ),
+          textAlign: TextAlign.center,
+        ),
+        const SizedBox(height: 41),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 1,
+              child: CustomButton(
+                backgroundColor: AppColors.gray,
+                textColor: AppColors.blackGray,
+                text: btnText == "Cancel" ? "Remove" : "Cancel",
+                isLoading: authPresenter.isLoading.value,
+                onPressed: onPressed,
+              ),
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              flex: 2,
+              child: CustomButton(
+                text: btnText,
+                isLoading: authPresenter.isLoading.value,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 18),
       ],
     );
   }
